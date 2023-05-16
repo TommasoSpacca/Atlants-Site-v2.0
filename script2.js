252e70a072b59fc4ca3cd4908f1ed0dae397775f
Luca Cassano <lcassano2007+account@gmail.com>
2023-05-16 19:52:41 +0200

    update

---
+++ script2.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var images = [
+        "vanilla1.png",
+        "vanilla2.png",
+        "vanilla3.png",
+        "vanilla4.png",
+        "vanilla5.png",
+        "vanilla6.png",
+        "vanilla7.png",
+        "vanilla8.png"
+      ];
+  
+    var currentIndex = 0;
+    var imageElement = document.getElementById("myImage");
+    var prevButton = document.getElementById("prevButton");
+    var nextButton = document.getElementById("nextButton");
+    var imageIndicator = document.getElementById("imageIndicator");
+  
+    function updateImageIndicator() {
+      var currentImageIndex = currentIndex + 1;
+      var totalImages = images.length;
+      imageIndicator.textContent = currentImageIndex + "/" + totalImages;
+    }
+  
+    prevButton.addEventListener("click", function() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      imageElement.src = images[currentIndex];
+      updateImageIndicator();
+    });
+  
+    nextButton.addEventListener("click", function() {
+      currentIndex = (currentIndex + 1) % images.length;
+      imageElement.src = images[currentIndex];
+      updateImageIndicator();
+    });
+  
+    updateImageIndicator();
+  });
+  
